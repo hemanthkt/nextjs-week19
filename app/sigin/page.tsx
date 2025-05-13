4ce12a7fe4f33f328695329b9a0468a4b47897d7
hemanthkt <hmkt1503@gmail.com>
2025-05-13 22:00:29 +0530

    first commi

---
+++ app/sigin/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Sigin() {
+  return (
+    <div>
+      Sigin
+      <input type="text" />
+      <button>Sgin in</button>
+    </div>
+  );
+}
